85178fc8ebf90b77234d7c11260bbf9251f61bb3
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2025-01-31 09:22:52 +0200

    Show Spotify search results

--- resources/views/livewire/spotify-widget.blade.php
+++ resources/views/livewire/spotify-widget.blade.php
@@ -1,3 +1,23 @@
 <div>
-    <h1>Spotify Widget</h1>
+    <input type="text" wire:model="query" placeholder="Search Spotify">
+    <button wire:click="spotifySearch">Search</button>
+
+    @if(session()->has('error'))
+        <p class="text-red-500">{{ session('error') }}</p>
+    @endif
+
+    <ul>
+        @foreach($results as $track)
+            <li>
+                <strong>{{ $track['name'] }}</strong> by {{ $track['artists'][0]['name'] }}
+                <br>
+                <p>{{ $track['preview_url'] }}</p>
+                <a href={{ $track['external_urls']['spotify'] }} target="_blank" rel="noopener noreferrer">Go</a>
+                <img src={{$track['album']['images'][0]['url']}} alt="">
+                <audio controls>
+                    <source src="{{ $track['preview_url'] }}" type="audio/mpeg">
+                </audio>
+            </li>
+        @endforeach
+    </ul>
 </div>
