d4f77a09006a9d63076ccf4e0a05b9d5bfc8819f
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2025-01-31 09:26:08 +0200

    Add searchCountry method to show Covid stats corresponding to the country

--- app/Livewire/CovidWidget.php
+++ app/Livewire/CovidWidget.php
@@ -2,10 +2,30 @@
 
 namespace App\Livewire;
 
+use Illuminate\Support\Facades\Http;
 use Livewire\Component;
 
 class CovidWidget extends Component
 {
+
+    public $country = '';
+    public $covidData = [];
+
+
+    public function searchCountry()
+    {
+        if (!empty($this->country)) {
+            $response = Http::get("https://disease.sh/v3/covid-19/countries/{$this->country}");
+
+            if ($response->successful()) {
+                $this->covidData = $response->json();
+                $this->dispatch('updateData', ['covidData' => $this->covidData]);
+            } else {
+                $this->covidData = [];
+            }
+        }
+    }
+
     public function render()
     {
         return view('livewire.covid-widget');
