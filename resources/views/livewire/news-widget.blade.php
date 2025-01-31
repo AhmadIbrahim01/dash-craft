11aeed8d5172bb087a63fc774c50e18f3c1e4030
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2025-01-31 08:54:58 +0200

    Add select input to choose news type

--- resources/views/livewire/news-widget.blade.php
+++ resources/views/livewire/news-widget.blade.php
@@ -1,3 +1,11 @@
 <div>
-    <h1>News</h1>
+    <div class="weather-container">
+        <h2 class="title">News Widget</h2>
+        <select wire:model="news" wire:change="showNews" class="city-select">
+            <option value="">Select news</option>
+            <option value="us">US</option>
+            <option value="tesla">Tesla</option>
+            <option value="apple">Apple</option>
+        </select>
+    </div>
 </div>
