6982d1def22338a66734675bd8c4c9c4acd407a5
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2025-01-31 08:47:01 +0200

    Show weather data after fetching weather API

--- resources/views/livewire/weather-widget.blade.php
+++ resources/views/livewire/weather-widget.blade.php
@@ -1,7 +1,7 @@
 <div>
     <div class="weather-container">
         <h2 class="title">Weather App</h2>
-        <select wire:model="location" wire:change="fetchFromApi" class="city-select">
+        <select wire:model="location" wire:change="showWeather" class="city-select">
             <option value="">Select a city</option>
             <option value="London">London</option>
             <option value="Paris">Paris</option>
@@ -12,5 +12,29 @@
 
     @if ($error)
         <div class="alert">{{ $error }}</div>
+    @endif
+
+    @if ($data)
+    @php
+        $sunset = $data['sys']['sunset'];
+        $sunrise = $data['sys']['sunrise'];
+        $date = $data['dt'];
+        
+        $formattedSunset = \Carbon\Carbon::createFromTimestamp($sunset)->format('h:i A');
+        $formattedSunrise = \Carbon\Carbon::createFromTimestamp($sunrise)->format('h:i A');
+        $formattedDate = \Carbon\Carbon::createFromTimestamp($date)->format('d-m-Y');
+    @endphp
+
+    <div class="weather-card">
+            <h3 class="weather-title">{{ $location }}</h3>
+            <p class="date">📅 Date: <span>{{ $formattedDate }}</span></p>
+            <p class="sun-time">🌅 Sunrise: <span>{{ $formattedSunrise }}</span></p>
+            <p class="sun-time">🌇 Sunset: <span>{{ $formattedSunset }}</span></p>
+            <p class="weather-info">🌡 Temperature: <span>{{ round($data['main']['temp'] - 273.15, 1) }}°C</span></p>
+            <p class="weather-info">🤔 Feels like: <span>{{ round($data['main']['feels_like'] - 273.15, 1) }}°C</span></p>
+            <p class="weather-info">💨 Wind Speed: <span>{{ $data['wind']['speed'] }} m/s</span></p>
+            <p class="weather-info">💧 Humidity: <span>{{ $data['main']['humidity'] }}%</span></p>
+            <p class="weather-info">👀 Visibility: <span>{{ $data['visibility'] / 1000 }} km</span></p>
+    </div>
     @endif
 </div>
