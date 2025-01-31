00ac1f8333e375083bc0156538e8d0d00c7ec8ab
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2025-01-31 08:56:52 +0200

    Add fetched news data

--- resources/views/livewire/news-widget.blade.php
+++ resources/views/livewire/news-widget.blade.php
@@ -13,4 +13,23 @@
     @if ($error)
         <div class="alert">{{ $error }}</div>
     @endif
+
+
+    @if ($data)
+        <div class="news-container">
+            @for ($i = 0; $i < 3; $i++) 
+                <div class="news-card">
+                    <img src="{{ $articles[$i]['urlToImage'] }}" alt="News Image" class="news-image">
+                    <div class="news-content">
+                        <h3 class="news-title">{{ $articles[$i]['title'] }}</h3>
+                        <p class="news-source"><strong>Source:</strong> {{ $articles[$i]['source']['name'] }}</p>
+                        <p class="news-author"><strong>Author:</strong> {{ $articles[$i]['author'] }}</p>
+                        <p class="news-description">{{ $articles[$i]['description'] }}</p>
+                        <a href="{{ $articles[$i]['url'] }}" target="_blank" class="news-link">Read More</a>
+                    </div>
+                </div>  
+            @endfor
+        </div>
+    @endif
+
 </div>
