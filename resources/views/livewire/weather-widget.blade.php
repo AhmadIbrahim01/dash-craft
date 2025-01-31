fee0dc7a121e3d6b48f4cb07afd6eb0b2d8969eb
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2025-01-31 08:39:18 +0200

    Add weather widget and select input inside it

---
+++ resources/views/livewire/weather-widget.blade.php
@@ -0,0 +1,11 @@
+<div>
+    <div class="weather-container">
+        <h2 class="title">Weather App</h2>
+        <select wire:model="location" wire:change="fetchFromApi" class="city-select">
+            <option value="">Select a city</option>
+            <option value="London">London</option>
+            <option value="Paris">Paris</option>
+            <option value="Los Angeles">Los Angeles</option>
+        </select>
+    </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,15 @@
-<div>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>DashCraft</title>
+    <link rel="stylesheet" href="{{ asset('css/weather.css') }}">
+
+</head>
+<body>
     <h1>Welcome to DashCraft</h1>
     <h2>My widgets</h2>
-</div>
+    <livewire:weather-widget/>
+</body>
+</html>
